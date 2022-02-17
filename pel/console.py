78ec2776f302c99fb8d6c9cb3e26efa4f3ef06d9
James Mishra <james.mishra@neocrym.com>
2022-02-17 01:09:00 -0600

    Bugfixes for finding files and setting namespaces

--- pel/console.py
+++ pel/console.py
@@ -5,13 +5,16 @@ The Pel command line entrypoint.
 When we run Pel from the command line, we always initialize
 the :py:attr:`pel.core.DEFAULT_GRAPH` .
 """
+import sys
+
 import pel.core
 
 pel.core.DEFAULT_GRAPH = pel.core.Graph()
 
 import pel.run  # pylint: disable=wrong-import-position
 
-run = pel.run.run
+def run():
+    sys.exit(pel.run.run())
 
 
 if __name__ == "__main__":
--- pel/run.py
+++ pel/run.py
@@ -5,6 +5,7 @@ This module is used by the :py:mod:`pel.console` module,
 which makes sure to set :py:attr:`pel.core.DEFAULT_GRAPH` first.
 
 """
+from pathlib import Path
 import typing
 
 import pel.core
@@ -16,10 +17,18 @@ def run(
     graph: typing.Optional[pel.core.Graph] = None,
     filename: str = "build.py",
     encoding: str = "utf-8",
-) -> None:
+) -> int:
     """Runs a Pel build file."""
-    with open(filename, encoding=encoding) as handle:
-        build_py = handle.read()
+    filepath = str((Path.cwd() / filename).absolute())
+    try:
+        with open(filepath, encoding=encoding) as handle:
+            build_py = handle.read()
+    except FileNotFoundError:
+        print(f"Could not find a Pel Build File at:")
+        print("\t", filepath, sep="")
+        print("\nNeed help writing a Build File?")
+        print("You can find instructions at https://github.com/neocrym/pel\n")
+        return 1
     exec(
         build_py,
         # Put our Pel Task constructor classes in our build script's
@@ -29,8 +38,7 @@ def run(
             for key, val in pel.tasks.__dict__.items()
             if key in pel.tasks.__all__
         },
-        # Do not pass anything to the LOCAL namespace.
-        {},
     )
     parser = pel.core.ArgParser(graph=graph)
     parser.interpret_args()
+    return 0
